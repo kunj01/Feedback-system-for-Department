7704199fb4fa43b5e812bd3db1d3e2625d6d5a18
Kunj Clg <23it028@charusat.edu.in>
2026-01-08 15:51:05 +0530

    Changes in vite

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,19 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    base: '/build/',
+    build: {
+        outDir: 'public/build',
+        emptyOutDir: true,
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
-        tailwindcss(),
     ],
 });
